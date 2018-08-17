9b7ba65d28c4ac5744a3e22805cfa48ee6f22039
Youri <youribok@msn.com>
2018-08-17 04:17:32 +0200

    Adds some string features; split a string on various special characters

---
+++ src/string.js
@@ -0,0 +1,20 @@
+/**
+ * Split the given string on the line feed character (LF)
+ * @param input {string} The string to split up
+ * @returns {*|string[]}
+ */
+export const splitLF = input => input.split(/\n/g);
+
+/**
+ * Split the given string on the carriage return + line feed character combo (CRLF)
+ * @param input {string} The string to split up
+ * @returns {*|string[]}
+ */
+export const splitCRLF = input => input.split(/\r\n/g);
+
+/**
+ * Split the given string on the tab character
+ * @param input {string} The string to split up
+ * @returns {*|string[]}
+ */
+export const splitTab = input => input.split(/\t/g);
