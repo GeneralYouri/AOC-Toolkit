6675960ce6161c46f58e29f483b1912950222be4
Oktay Dinler <24858380+aoktayd@users.noreply.github.com>
2018-08-18 17:36:19 +0200

    Refactors makeObjectIterable explicit return

--- src/object.js
+++ src/object.js
@@ -2,13 +2,11 @@
  * Creates an iterable from object
  * @param {Object} object - The object to create the iterator from
  */
-export const makeObjectIterable = (object) => {
-    return {
-        * [Symbol.iterator]() {
-            for (const key of Object.keys(object)) {
-                const value = object[key];
-                yield [key, value];
-            }
-        },
-    };
-};
+export const makeObjectIterable = object => ({
+    * [Symbol.iterator]() {
+        for (const key of Object.keys(object)) {
+            const value = object[key];
+            yield [key, value];
+        }
+    },
+});
