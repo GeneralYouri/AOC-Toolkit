01d5b1918134451abcd4aaadee58f12321688250
GeneralYouri <youribok@msn.com>
2018-11-07 18:36:59 +0100

    Fixes `rollup -w` behavior and adds npm script `dev` for easy continuous development and testing

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
+import builtins from 'rollup-plugin-node-builtins';
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
 export default {
@@ -8,7 +10,9 @@ export default {
         format: 'cjs',
     },
     plugins: [
+        builtins(),
         resolve(),
+        commonjs(),
         babel({
             exclude: 'node_modules/**',
         }),
