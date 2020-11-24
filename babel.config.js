797c3c5cff685575696f0c051738aeb8d2ac035a
GeneralYouri <youribok@msn.com>
2020-11-24 16:11:52 +0100

    [1] Applies a major cleanup of packages and updates to outdated packages

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = (api) => {
+    // true when running tests via Jest
+    const isTest = api.env('test');
+
+    api.cache(true);
+
+    const presets = [
+        ['@babel/preset-env', { targets: { node: 'current' } }]
+    ];
+    const plugins = [
+        '@babel/plugin-syntax-bigint',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+        '@babel/plugin-proposal-optional-chaining',
+        '@babel/plugin-proposal-private-methods'
+    ];
+
+    return {
+        presets,
+        plugins
+    };
+};
